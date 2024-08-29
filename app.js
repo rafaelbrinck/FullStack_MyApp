5d7ec6606c877781b9acbf1e26eee5df6057632b
Rafael Brinckmann <rafabrinckmann@gmail.com>
2024-08-28 21:56:05 -0300

    Passo 3 - Atualizando Projeto

--- app.js
+++ app.js
@@ -2,28 +2,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const produtos = [
+  {id:1, nome:'Arroz', preço:10, categoria:'Alimento'},
+  {id:2, nome:'Feijão', preço:20, categoria:'Alimento'},
+  {id:3, nome:'Suco', preço:8, categoria:'Bebidas'}
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(produtos)
 })
 
 app.get('/produtos', (req, res) => {
-  res.send('Listar Produtos')
+  res.send(produtos)
 })
 
 app.get('/produtos/:id', (req, res) => {
-  res.send('Buscar Produtos por ID')
+  const id = req.params.id 
+  res.status(201).json(produtos[id])
 })
 
-app.post('/produtos', (req, res) =>{
-  res.send('Inserir Produto');
-});
+app.post('/produtos', (req, res) => {
+  const produto = req.body
+  console.log(produto)
+  res.json(produto)
+})
+
 
 app.put('/produtos/:id', (req, res) =>{
-  res.send('Atualizar Produtos');
+  const produto = req.body
+  const id = req.params.id
+  console.log(produto)
+
+  res.status(201).json(produto[id])
 });
 
 app.delete('/produtos/:id', (req, res) =>{
-  res.send('Deletar Produtos');
+  const id = req.params.id
+
+  res.status(201).json(produtos[id])
 });
 
 app.listen(port, () => {
